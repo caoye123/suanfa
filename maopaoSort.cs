ae9113fe1ab8a93bd4a1f3d814ff4932deec352a
一亿次的胡思乱想 <44029790+caoye123@users.noreply.github.com>
2023-09-19 13:14:13 +0800

    Add files via upload

---
+++ maopaoSort.cs
@@ -0,0 +1,30 @@
+﻿//
+
+int[] array = {232,22,5,9,1,7,3};
+BSort(array);
+foreach (int i in array)
+{
+    Console.Write(i+" ");
+}
+
+static void BSort(int[] array) {
+    var length = array.Length;
+    for (int i = 0; i < length - 1; i++) {
+        bool swap = false;
+        int temp = 0;
+        for (int j = 0; j < length - i - 1; j++)
+        {
+            if(array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                swap = true;
+            }
+        }
+        if (!swap)
+        {
+            break;
+        }
+    }
+}
