64e618fadd8a7c29414ed125ae91603071c57acb
caoye <875944004@qq.com>
2023-09-20 18:24:29 +0800

    冒泡排序更新

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
+/*int b = 51202;
+string a = string.Format("{0:N0}",b);
+string a2 = b.ToString("N");
+Console.WriteLine(a);*/
